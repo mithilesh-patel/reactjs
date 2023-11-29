73b74cb421bc0ba3d3b49df61d7865afa4d8665c
mithileshpatel <mithileshpatel250@gmail.com>
2023-11-29 07:58:45 +0530

    Creating nested structure using createElement

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
-const heading = React.createElement("h1", {}, "Hello Mithilesh from React");
+// const heading = React.createElement("h1", {}, "Hello Mithilesh from React");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>This is a h1 tag</h1>
+    </div>
+</div> */
+}
+
+// The three arguments of createElement are tagname, attributes and children
+// So in the children if we want to give any child elements we can create using createElement
+
+const heading = React.createElement("div",{ id: "parent" },React.createElement("div",{ id: "child" },React.createElement("h1", {}, "This is a h1 tag")));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);

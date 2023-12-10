fc99a6656c4567a8576a044ef1661be2273e4973
mithileshpatel <mithileshpatel250@gmail.com>
2023-12-10 17:10:38 +0530

    adding js to Component using curly bracket

--- App.js
+++ App.js
@@ -4,16 +4,26 @@ import ReactDOM from "react-dom/client";
 const Heading = React.createElement(
   "h1",
   { id: "using Core React" },
-  "This is using core React"
+  "This is using core React "
 );
 
 const JsxHeading = () => (
     <h1>This is using JSX 🚀</h1>
 );
 
+const Body = ()=> (
+    <h1>This is Body 💪😎 Component</h1>
+)
+
+const naam= "Mithilesh";
+
 const Headingcomponent = () => (
   <div id="container">
     <JsxHeading />
+    {Heading}
+    {100+300}
+    {console.log("This is inside console")}
+    <Body />
     <h1>🌸 This is the Heading1 Component 🌸</h1>
     <h2>🌸 This is the Heading2 Component 🌸</h2>
   </div>

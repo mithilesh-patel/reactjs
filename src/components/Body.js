3c653670c38d02b1104741d18b5e65395c45b33a
mithileshpatel <mithileshpatel250@gmail.com>
2024-01-04 23:57:00 +0530

    Set the Restaurant list as default list

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,72 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
 const Body = () => {
-  const [listofRes, setlistofRes] = useState([{
-    info: {
-      id: "3241",
-      name: "Meghana Foods",
-      cloudinaryImageId: "e0vvulfbahjxjz6k4uwi",
-      locality: "Residency Road",
-      areaName: "Residency Road",
-      costForTwo: "₹500 for two",
-      cuisines: [
-        "Biryani",
-        "Andhra",
-        "South Indian",
-        "North Indian",
-        "Chinese",
-        "Seafood",
-      ],
-      avgRating: 3.8,
-    }
-
-
-  },
-
-  {
-    info: {
-      id: "3241",
-      name: "KFC",
-      cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-      locality: "Residency Road",
-      areaName: "Residency Road",
-      costForTwo: "₹500 for two",
-      cuisines: [
-        "Biryani",
-        "Andhra",
-        "South Indian",
-        "North Indian",
-        "Chinese",
-        "Seafood",
-      ],
-      avgRating: 4.6,
-    }
-
-
-  },
-  {
-    info: {
-      id: "3241",
-      name: "MFC",
-      cloudinaryImageId: "nfieyqgxk1omknxfbluj",
-      locality: "Residency Road",
-      areaName: "Residency Road",
-      costForTwo: "₹500 for two",
-      cuisines: [
-        "Biryani",
-        "Andhra",
-        "South Indian",
-        "North Indian",
-        "Chinese",
-        "Seafood",
-      ],
-      avgRating: 4.3,
-    }
-
-
-  }
-
-  ])
+  const [listofRes, setlistofRes] = useState(RES_LIST)
 
   return (
     <div className="body">
